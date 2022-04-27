f8761ec0b1382a774e2c6cdb9e02eeeb1f6de23a
dacosta <jldboss@live.fr>
2022-04-27 19:09:29 +0200

    version 6 2704 page prod/image

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,25 @@
-const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
-const urlSearchParams = new URLSearchParams(queryString_url_id);
-console.log(urlSearchParams);
+let urlId = new URL(window.location.href);
+let productId = urlId.searchParams.get("id");
+
+
+const productsUrl = "http://localhost:3000/api/products/";
+
+async function getArticle(productId) {
+    const catchArticle = await fetch(productsUrl + productId)
+        .then((catchArticle) => catchArticle.json())
+        .then(function(data) {
+            article = data;
+        })
+    return (article);
+}
+
+async function displayArticle(productId) {
+    const article = await getArticle(productId);
+
+    const productImg = document.createElement("img");
+    document.querySelector(".item__img").appendChild(productImg);
+    productImg.src = article.imageUrl;
+    productImg.alt = article.altTxt;
+
+}
+displayArticle(productId);
